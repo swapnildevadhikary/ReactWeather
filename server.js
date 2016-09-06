5252ffc0962896998d7fe958d9043ad45e351878
Adhikary <SAdhikary@ad.harman.com>
2016-09-06 12:52:05 +0530

    Prep repo for heroku

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var  exp1=require('express');
 var app=exp1();
+const PORT=process.env.PORT||4000;
+
+app.use(function(req,res,next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect('http://'+req.hostname+req.url);
+  }
+});
 app.use(exp1.static('public'));
-app.listen(4000,function(){
-  console.log('hello my first app');
+app.listen(PORT,function(){
+  console.log('hello my first app is up on port'+PORT);
 })

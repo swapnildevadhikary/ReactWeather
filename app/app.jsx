ae49a085dea282b8161be63ca900909b7c88eb4f
Adhikary <SAdhikary@ad.harman.com>
2016-09-06 22:39:15 +0530

    Add New Styles

--- app/app.jsx
+++ app/app.jsx
@@ -12,6 +12,8 @@ var Example = require('Example');
 require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
+require('style!css!applicationStyles')
+
 
 ReactDOM.render(
   <Router history={hashHistory}>
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -12,7 +12,7 @@ var WeatherForm = React.createClass({
     return(
       <div>
         <form onSubmit={this.onFormSubmit}>
-        <input type="text" ref="location"/>
+        <input type="search" ref="location" placeholder="Search Weather By City"/>
         <button className="button expanded hollow">Get Weather</button>
         </form>
       </div>

a198afc2ef21edab51152f10ebc5ef2b16c58418
Adhikary <SAdhikary@ad.harman.com>
2016-09-06 16:51:33 +0530

    Update button style

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,7 +4,7 @@ var React =require('react');
 var WeatherMessage = (props) => {
   var {temp,location} = props;
   return(
-    <h2>It's it {temp} in {location}.</h2>
+    <h3 className="text-center">It's it {temp} in {location}.</h3>
   )
 };
 
